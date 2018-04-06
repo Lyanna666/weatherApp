902dcab83aa4c8ba5e15e7492a170fd7b9d7c478
Andrea González García <ggarciaandrea@gmail.com>
2018-04-06 17:16:57 +0200

    First Commit

---
+++ weatherApp/weatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by Andrea González García on 6/4/18.
+//  Copyright © 2018 Andrea González García. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
